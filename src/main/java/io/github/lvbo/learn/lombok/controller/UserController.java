3610f1bd1daab4b1d1dfefa26de7285c332e276c
lvbo <lvbo09@gmail.com>
2019-09-12 10:21:10 +0800

    增加了@Slf4j的使用

--- src/main/java/io/github/lvbo/learn/lombok/controller/UserController.java
+++ src/main/java/io/github/lvbo/learn/lombok/controller/UserController.java
@@ -3,6 +3,7 @@ package io.github.lvbo.learn.lombok.controller;
 import com.alibaba.fastjson.JSON;
 import io.github.lvbo.learn.lombok.model.User;
 import io.github.lvbo.learn.lombok.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/user")
+@Slf4j
 public class UserController {
 
     @Autowired
@@ -21,6 +23,7 @@ public class UserController {
 
     @PostMapping
     public void create(@RequestParam String user) {
+        log.info("user is {}", user);
         userService.createUser(JSON.parseObject(user, User.class));
     }
 }
